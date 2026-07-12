de5ae6bedcfbd01c47ae86bfd58664d009bfd2f5
kerorady2018-png <kerorady2018@gmail.com>
2026-07-12 17:27:58 +0300

    Add files via upload

---
+++ api/rates.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+    try {
+        const filePath = path.join(process.cwd(), 'rates.json');
+        const data = fs.readFileSync(filePath, 'utf8');
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
