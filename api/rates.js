6041d4c1ac07075d5a3aa65950f2f144609d55aa
kerorady2018-png <kerorady2018@gmail.com>
2026-07-20 19:59:52 +0300

    Update rates.js

--- api/rates.js
+++ api/rates.js
@@ -16,6 +16,19 @@ export default async function handler(req, res) {
       "GBP": 60.20,
       "SAR": 12.90,
       "AED": 13.20,
+      "KWD": 157.80,
+      "BHD": 128.60,
+      "OMR": 126.00,
+      "QAR": 13.30,
+      "JOD": 68.40,
+      "CHF": 55.20,
+      "JPY": 0.31,
+      "CAD": 35.40,
+      "AUD": 32.10,
+      "CNY": 6.70,
+      "SEK": 4.60,
+      "NOK": 4.50,
+      "DKK": 7.00,
       "EGP": 1
     };
     
