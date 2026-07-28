1afafb43a4dee688396eed80305b4e51ac8b6f12
kerorady2018-png <kerorady2018@gmail.com>
2026-07-28 23:50:10 +0300

    Update silver.js

--- api/silver.js
+++ api/silver.js
@@ -3,6 +3,13 @@ let lastFetchTime = 0;
 const UPDATE_INTERVAL = 15 * 60 * 1000; // 15 دقيقة
 
 module.exports = async (req, res) => {
+  // السماح بالوصول من أي مصدر (CORS)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  
+  // تفعيل التخزين المؤقت على حافة الشبكة لمدة 15 دقيقة (900 ثانية) للفضة
+  res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=300');
+
   const nowTime = Date.now();
 
   // التخزين المؤقت على السيرفر لتوفير الطلبات للفضة
